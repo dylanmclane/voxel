246f03341d2de17ee030898f06ac6c5f444a1f63
dylanmclane <mclane_dylan@yahoo.com>
2024-01-31 14:26:39 -0800

    initial commit

---
+++ react-voxel-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import VoxelScene from './VoxelScene';
+
+function App() {
+  return (
+    <div className="App">
+      <VoxelScene />
+    </div>
+  );
+}
+
+export default App;
